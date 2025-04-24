2a216c7f3f0efdc03b8e41eb8fb2f9ada6184de2
ykan <nameyukan@gmail.com>
2025-04-24 16:55:16 +0800

    wip: add some components

--- app/[locale]/[workspace]/ticket/list/page.tsx
+++ app/[locale]/[workspace]/ticket/list/page.tsx
@@ -4,30 +4,25 @@ import { useQuery } from '@tanstack/react-query'
 import { request } from '@/lib/request'
 import { Tables } from '@/lib/supabase.types'
 
-// 获取工单列表
-export function useTickets() {
-  return useQuery({
+export default function TicketList() {
+  const t = useTranslations('ticket.list')
+  const { data: list } = useQuery({
     queryKey: ['tickets'],
     queryFn: async () => {
       const res = await request.get('/ticket/list')
       return res.data
     },
   })
-}
-
-export default function TicketList() {
-  const t = useTranslations('ticket.list')
-  const { data: list } = useTickets()
   return (
-    <div className="p-5">
-      <div>{t('title')}</div>
-      <ul>
+    <div>
+      <div className="px-4 pt-3 pb-2 border-b-[0.5px]">{t('title')}</div>
+      <ul className="px-4 pt-3">
         {list?.map((item: Tables<'ticket'>) => (
           <li key={item.id}>
-            <span>{item.title}</span>
-            <span>{item.desc}</span>
-            <span>{item.status}</span>
-            <span>{item.priority}</span>
+            <span className="px-1">{item.priority}</span>
+            <span className="px-1">{item.status}</span>
+            <span className="px-1">{item.title}</span>
+            <span className="px-1">{item.desc}</span>
           </li>
         ))}
       </ul>
